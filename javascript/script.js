15fc3ad0c787d08b3317293c216df9ef0cafdeff
Dusan Krstic <krstic.duci@gmail.com>
2017-06-02 17:56:20 +0200

    added code to .js, global.scss & .html

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,25 @@
 $(function () {
 
+  // on 480px make new breakpoints
+  $(window).resize(function() {
+    if($(window).width() < 463) {
+
+      // section divs (features, designs, products)
+      $('.custom-col-xxs').removeClass('col-xs-6').addClass('col-xs-12');
+
+      // carousel img and para
+      $('.custom-col-crls-xxs').removeClass('col-xs-4').addClass('col-xs-12');
+      $('.custom-col-crls-para-xxs').removeClass('col-xs-8').addClass('col-xs-12');
+    } else {
+
+      // section divs (features, designs, products)
+      $('.custom-col-xxs').removeClass('col-xs-12').addClass('col-xs-3');
+
+      // carousel img and para
+      $('.custom-col-crls-xxs').removeClass('col-xs-12').addClass('col-xs-4');
+      $('.custom-col-crls-para-xxs').removeClass('col-xs-12').addClass('col-xs-8');
+    }
+  });
 });
+
+//slika crls

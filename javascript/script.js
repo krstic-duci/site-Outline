08becc04bf0a51c889855e8be6d2c58220da05ff
Dusan Krstic <krstic.duci@gmail.com>
2017-06-03 12:01:03 +0200

    fixed aos in faq section in .html

--- javascript/script.js
+++ javascript/script.js
@@ -25,45 +25,29 @@ $(function () {
     }
   });
 
-  // on mouse click, rotate caret arrows by 180deg
-
-  // when accoridon elements has clas in (that means that elem is visible)
-  // add bg color to panel-heading to match color with para below it
-
-
   // trigger animation on count up number when scroll to this article
-  // function triggerCountUp () {
-
-  //   var targetOffset = $('.wrapper-numbers-and-bg-img').offset().top;
-
-  //   var $w = $(window).scroll(function(){
-
-  //     if ( $w.scrollTop() < targetOffset ) {   
-  //       $({countNum: 1}).animate({countNum: 28}, {
-  //       duration: 4000,
-  //       easing:'linear',
-  //       step: function() {
-  //         $('.num-one').text(Math.round(this.countNum));
-  //       }
-  //       });
-  //       $({countNum: 1}).animate({countNum: 57}, {
-  //         duration: 4000,
-  //         easing:'linear',
-  //         step: function() {
-  //           $('.num-two').text(Math.round(this.countNum));
-  //         }
-  //       });
-  //       $({countNum: 1}).animate({countNum: 34023}, {
-  //         duration: 4000,
-  //         easing:'linear',
-  //         step: function() {
-  //           $('.num-three').text(Math.round(this.countNum.toFixed(2)));
-  //         }
-  //       });
-  //     }
-  //   });
-  // }
-  // triggerCountUp();
+    // $({countNum: 1}).animate({countNum: 28}, {
+    // duration: 4000,
+    // easing:'linear',
+    // step: function() {
+    //   $('.num-one').text(Math.round(this.countNum));
+    // }
+    // });
+    // $({countNum: 1}).animate({countNum: 57}, {
+    //   duration: 4000,
+    //   easing:'linear',
+    //   step: function() {
+    //     $('.num-two').text(Math.round(this.countNum));
+    //   }
+    // });
+    // $({countNum: 1}).animate({countNum: 34023}, {
+    //   duration: 4000,
+    //   easing:'linear',
+    //   step: function() {
+    //     $('.num-three').text(Math.round(this.countNum.toFixed(2)));
+    //   }
+    // });
+    
 });
 
 // Initialize Animate On Scroll 

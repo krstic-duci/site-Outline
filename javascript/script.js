fc79627671250ff73face9ef26136215f937a968
Dusan Krstic <krstic.duci@gmail.com>
2017-06-04 14:31:13 +0200

    removed count up number function

--- javascript/script.js
+++ javascript/script.js
@@ -23,31 +23,9 @@ $(function () {
       $('.custom-col-crls-para-xxs')
       .removeClass('col-xs-12').addClass('col-xs-8');
     }
-  });
 
-  // trigger animation on count up number when scroll to this article
-    // $({countNum: 1}).animate({countNum: 28}, {
-    // duration: 4000,
-    // easing:'linear',
-    // step: function() {
-    //   $('.num-one').text(Math.round(this.countNum));
-    // }
-    // });
-    // $({countNum: 1}).animate({countNum: 57}, {
-    //   duration: 4000,
-    //   easing:'linear',
-    //   step: function() {
-    //     $('.num-two').text(Math.round(this.countNum));
-    //   }
-    // });
-    // $({countNum: 1}).animate({countNum: 34023}, {
-    //   duration: 4000,
-    //   easing:'linear',
-    //   step: function() {
-    //     $('.num-three').text(Math.round(this.countNum.toFixed(2)));
-    //   }
-    // });
-    
+ 
+  });
 });
 
 // Initialize Animate On Scroll 

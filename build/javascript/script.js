d8a4463ee226b026a3ebaea27637a7407f15a84a
Dusan Krstic <krstic.duci@gmail.com>
2017-09-10 20:47:43 +0200

    Set accordion to work; remove prop in aos js

--- build/javascript/script.js
+++ build/javascript/script.js
@@ -1,17 +1,17 @@
-$(function () {
+$(function() {
 
   // on 480px make new breakpoints
   $(window).resize(function() {
-    if($(window).width() < 463) {
+    if ($(window).width() < 463) {
 
       // section divs (features, designs, products)
       $('.custom-col-xxs').removeClass('col-xs-6').addClass('col-xs-12');
 
       // carousel img and para
       $('.custom-col-crls-xxs')
-      .removeClass('col-xs-4').addClass('col-xs-12');
+        .removeClass('col-xs-4').addClass('col-xs-12');
       $('.custom-col-crls-para-xxs')
-      .removeClass('col-xs-8').addClass('col-xs-12');
+        .removeClass('col-xs-8').addClass('col-xs-12');
     } else {
 
       // section divs (features, designs, products)
@@ -19,19 +19,61 @@ $(function () {
 
       // carousel img and para
       $('.custom-col-crls-xxs')
-      .removeClass('col-xs-12').addClass('col-xs-4');
+        .removeClass('col-xs-12').addClass('col-xs-4');
       $('.custom-col-crls-para-xxs')
-      .removeClass('col-xs-12').addClass('col-xs-8');
+        .removeClass('col-xs-12').addClass('col-xs-8');
     }
+
+  });
+
+  // Set open headingOne background to white
+  $('#headingOne').css('background', '#fff');
+
+  // Set inital state of chevron arrow
+  $('#headingOne').addClass('active-chevron');
+
+  // Set color to open accordion
+  $('#accordion').on('shown.bs.collapse', function() {
+
+    $('.panel-collapse').each(function() {
+
+      // Four quicker usage
+      var $thisHeading = $(this).prev('.panel-heading');
+
+      if ($(this).hasClass('in')) {
+
+        // When accordion is open set proper bg color
+        $thisHeading.css('background', '#fff');
+
+        // Change inital state of chevron arrow
+        $thisHeading.addClass('active-chevron');
+
+      } else {
+        $(this).prev('.panel-heading').css('background', '#fafafa');
+      }
+    });
+    
+  });
+
+  // Set color to hidden accordion
+  $('#accordion').on('hidden.bs.collapse', function() {
+
+    //
+    var $panelCollHeading = $('.panel-collapse').prev('.panel-heading');
+
+    // When accordion is hidden set proper bg color
+    $panelCollHeading.css('background', '#fafafa');
+
+    // Change inital state of chevron arrow
+    $panelCollHeading.removeClass('active-chevron');
 
   });
 
 });
 
 // Initialize Animate On Scroll 
-$(function () {
+$(function() {
   AOS.init({
-    once: true,
-    disable: window.innerWidth < 1024
+    once: true
   });
 });
